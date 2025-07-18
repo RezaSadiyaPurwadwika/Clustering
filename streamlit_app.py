c746f5a132988dd48a63953e2a1223b0427bccd3
RezaSadiyaPurwadwika <113271200+RezaSadiyaPurwadwika@users.noreply.github.com>
2025-07-18 10:09:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,6 +58,19 @@ if menu == "🏠 Home":
         - `jenis`: "mamin" / "oleh"
         """)
 
+# =============== UPLOAD ===============
+elif menu == "📂 Upload Data":
+    st.title("📂 Upload Dataset UMKM")
+    uploaded_file = st.file_uploader("Unggah file CSV", type="csv")
+    if uploaded_file:
+        try:
+            df = pd.read_csv(uploaded_file)
+            st.session_state.df = df
+            st.success("✅ File berhasil diunggah!")
+            st.dataframe(df)
+        except Exception as e:
+            st.error(f"Terjadi kesalahan saat membaca file: {e}")
+
 # =============== ANALISIS DATA ===============
 elif menu == "📈 Analisis Data":
     st.title("📈 Analisis Data Awal")
@@ -83,20 +96,7 @@ elif menu == "📈 Analisis Data":
             st.subheader("2. Statistik Deskriptif")
             cols_num = ['omset', 'tenaga kerja', 'modal']
             st.dataframe(df[cols_num].describe())
-
-# =============== UPLOAD ===============
-elif menu == "📂 Upload Data":
-    st.title("📂 Upload Dataset UMKM")
-    uploaded_file = st.file_uploader("Unggah file CSV", type="csv")
-    if uploaded_file:
-        try:
-            df = pd.read_csv(uploaded_file)
-            st.session_state.df = df
-            st.success("✅ File berhasil diunggah!")
-            st.dataframe(df)
-        except Exception as e:
-            st.error(f"Terjadi kesalahan saat membaca file: {e}")
-
+            
 # =============== PREPROCESSING ===============
 elif menu == "⚙️ Data Preprocessing":
     st.title("⚙️ Tahap Preprocessing Data")
