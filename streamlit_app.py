6c7cd719a0204674b15b9b3ffaab105a90087754
RezaSadiyaPurwadwika <113271200+RezaSadiyaPurwadwika@users.noreply.github.com>
2025-07-19 20:41:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,16 +86,10 @@ elif menu == "⚙ Data Preprocessing":
 
             st.subheader("2. Boxplot Sebelum Outlier Handling")
             fig3, ax3 = plt.subplots()
-            sns.boxplot(data=df[['omset', 'modal']], ax=ax3)
+            sns.boxplot(data=df[cols_num], ax=ax3)
             st.pyplot(fig3)
 
-            # Tambahkan boxplot khusus tenaga kerja
-            st.markdown("**Boxplot Variabel Tenaga Kerja**")
-            fig_tk, ax_tk = plt.subplots()
-            sns.boxplot(y=df['tenaga kerja'], ax=ax_tk)
-            st.pyplot(fig_tk)
-
-            for col in ['omset', 'modal', 'tenaga kerja']:
+            for col in cols_num:
                 Q1 = df[col].quantile(0.25)
                 Q3 = df[col].quantile(0.75)
                 IQR = Q3 - Q1
@@ -105,7 +99,7 @@ elif menu == "⚙ Data Preprocessing":
 
             st.subheader("3. Boxplot Setelah Outlier Handling")
             fig4, ax4 = plt.subplots()
-            sns.boxplot(data=df[['omset', 'modal']], ax=ax4)
+            sns.boxplot(data=df[cols_num], ax=ax4)
             st.pyplot(fig4)
 
             st.subheader("4. Normalisasi Data Z-Score")
